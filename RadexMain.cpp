0ba11221392118a8afd38b1228ab26761194eca1
Millisman <git@arduino.uno>
2022-11-23 07:00:41 +0100

    ui

--- RadexMain.cpp
+++ RadexMain.cpp
@@ -2,12 +2,14 @@
 #include "AppRadio/Radio.h"
 #include "AppSensor/Sensor.h"
 #include "AppUI/LiquidCrystal.h"
+#include "AppUI/UI.h"
 
 
 
 void setup(void) {
     Serial.begin(115200);
     Serial.println(F("\n\rRadex"));
+    setup_UI();
     //-----------------
     setup_radio();
     setup_sensors();
@@ -16,21 +18,10 @@ void setup(void) {
     do_sensors();
 }
 
-// static uint32_t time_now = 0;
-
 void loop(void) {
-
-//     if((millis() - time_now) > 1000) {
-//         time_now = millis();
-//         do_radio();
-//         do_sensors();
-//     }
-    
+    do_UI();
     do_radio();
     do_sensors();
-    
-    
-    
     // do sleep
     // TODO timer2 - sleep
 }
